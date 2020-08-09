0b5eb5ad2dfcee0688e0b261383ed6db086d3de6
Youngsun Paik <youngsunpaik@gmail.com>
2020-08-09 11:10:40 -0700

    clean up

--- App.js
+++ App.js
@@ -1,29 +1,14 @@
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { GlobalProvider } from './context/GlobalState'
-import { SafeAreaView, View, FlatList } from 'react-native'
+import { SafeAreaView, View } from 'react-native'
 import tailwind from 'tailwind-rn'
 import Header from './components/header'
 
 import TodoList from './components/todoList'
 import AddTodo from './components/addTodo'
-import { uuidv4 } from './utils/uuid'
 
 export default function App() {
-
-  // const [todos, setTodos] = useState([
-  //   { text: "Buy coffee", key: uuidv4() },
-  //   { text: "Create an app", key: uuidv4() },
-  //   { text: "Meditate", key: uuidv4() },
-  // ])
-
-  // const submitHandler = (text) => {
-  //   const newTodo = { text: text, id: uuidv4() }
-  //   console.log(newTodo.id)
-  //   setTodos(prevTodos => {
-  //     return [newTodo, ...prevTodos]
-  //   })
-  // }
 
   return (
     <GlobalProvider>
