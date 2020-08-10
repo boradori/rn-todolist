9b463c820507e2e3c2ed3ad77850531eb0af4454
Youngsun Paik <youngsunpaik@gmail.com>
2020-08-09 19:25:30 -0700

    vector icon and use of flexbox for styling button with delete icon and text

--- components/todoItem.js
+++ components/todoItem.js
@@ -1,8 +1,9 @@
 import React, { useContext } from 'react'
-import { Text } from 'react-native'
+import { View, Text } from 'react-native'
 import tailwind from 'tailwind-rn'
 import styled from 'styled-components/native'
 import { getColor } from 'tailwind-rn'
+import { AntDesign } from '@expo/vector-icons'
 
 import { GlobalContext } from '../context/GlobalState'
 
@@ -17,10 +18,13 @@ const TodoItem = ({ item }) => {
 
   return (
     <Button
-      style={tailwind('justify-center rounded-lg h-12 mt-4 p-8 bg-pink-300')}
+      style={tailwind('justify-center rounded-lg h-12 mt-4 px-4 py-6 bg-pink-300')}
       onPress={() => deleteTodo(item._id)}
     >
-      <Text style={tailwind('text-white self-center text-2xl font-bold')}>{item.text}</Text>
+      <View style={tailwind("flex-row justify-between items-center")}>
+        <Text style={tailwind('text-white self-center text-2xl font-bold')}>{item.text}</Text>
+        <AntDesign name="delete" size={24} color="white" />
+      </View>
     </Button>
   )
 }
